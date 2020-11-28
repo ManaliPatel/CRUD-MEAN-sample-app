2de5c75d27afd460948eb3b2ccc2396670ca08dc
Manali Patel <manali.patel@autodesk.com>
2020-11-27 21:45:01 -0500

    my first react node mongoDB project

---
+++ server/index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+const FoodeModel = require('./models/Food');
+const Food = require('./models/Food');
+
+app.use(express.json())
+app.use(cors());
+mongoose.connect(
+    "mongodb+srv://newuser:manali1234@cluster0.usaud.mongodb.net/food?retryWrites=true&w=majority", 
+   {
+    useNewUrlParser: true,
+   }
+);
+
+app.post("/insert", async(req,  res) => {
+    const foodName = req.body.foodName
+    const days = req.body.days
+
+    const food = new FoodeModel({ foodName: foodName, daysSinceIAte: days});
+    try{
+        await food.save();
+        res.send("inserted data");
+    } catch(err){
+        console.log(err);
+    }
+});
+
+app.put("/update", async(req,  res) => {
+    const newFoodName = req.body.newFoodName;
+    const id = req.body.id;
+    
+    try{
+       await FoodeModel.findById(id, (err, updatedFood) => {
+            updatedFood.foodName = newFoodName
+            updatedFood.save();
+            res.send('update');
+        })
+    } catch(err){
+        console.log(err);
+    }
+});
+
+app.get("/read", async(req,  res) => {
+    FoodeModel.find({}, (err, result) => {
+        if(err){
+            res.send(err)
+        }
+        res.send(result)
+    })
+});
+
+app.delete("/delete/:id", async(req,res) => {
+  const id = req.params.id;
+  
+  await FoodeModel.findByIdAndRemove(id).exec();
+  res.send("deleted");
+})
+app.listen(3001,() =>{
+    console.log('Server running on port 3001...')
+});
